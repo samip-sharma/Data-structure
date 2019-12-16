8cc3ab14bec996b3e6de883169b1385282ede842
samip-sharma <samip.sharma963@gmail.com>
2019-12-15 22:29:56 -0500

    merge sort done

--- merge-sort.js
+++ merge-sort.js
@@ -1,3 +1,11 @@
+function mergeSort(arr){
+    if (arr.length <=1 ) return arr
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0,mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+}
+
 
 function merge(left, right){
     let result=[]
@@ -21,6 +29,6 @@ function merge(left, right){
         j++
     }
 
-    return results
+    return result
 
 }
