1fc28f408cec9c86b1e87b5292719be82a896875
samip-sharma <samip.sharma963@gmail.com>
2019-12-22 17:56:38 -0500

    count integer in multidimentional array

--- Recursion.js
+++ Recursion.js
@@ -48,3 +48,31 @@ function productOfAArray(arr){
 
     return result
 }
+
+
+
+
+// Given a multi-dimensional integer array, return the total number of integers stored inside this array
+
+// Sample:
+
+// var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+
+
+var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+console.log(seven);
+
+function totalIntegers(array){
+	if(array.length === 0) return 0;
+
+	let total = 0;
+	let first = array.shift();
+
+	if (Array.isArray(first)){
+		total += totalIntegers(first); 
+	} else if (Number.isInteger(first)) {
+		total += 1;
+	}
+
+	return total + totalIntegers(array);
+}
