00e1c3882f6ccd7e64f7b4eefab16b2bcce3caad
samip-sharma <samip.sharma963@gmail.com>
2019-12-29 11:11:23 -0500

    replicate array

--- Recursion.js
+++ Recursion.js
@@ -104,3 +104,12 @@ function SumSquare(arr){
 
     return sum
 }
+
+// The function should return an array containing repetitions of the number argument. For instance,
+//  replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+
+function replicate(times, number){
+    if (times <= 0) return [];
+    
+    return [number].concat( replicate(times-1, number))    
+}
