a621682878b7fa6fe2abd6d514c80282b6956b96
samip-sharma <samip.sharma963@gmail.com>
2019-12-29 23:02:42 -0500

    selection sort

---
+++ SelectionSort.js
@@ -0,0 +1,20 @@
+function sselectionSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        var lowest = i;
+        for(var j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            //SWAP!
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
+}
+
+
+selectionSort([0,2,34,22,10,19,17]);
